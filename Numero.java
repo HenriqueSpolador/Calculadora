c7b31cede6b6658d70c023b89c8c719e9e5ac600
Henrique Marquezani Spolador <96837425+HenriqueSpolador@users.noreply.github.com>
2022-01-25 21:56:30 -0300

    Add files via upload

---
+++ Numero.java
@@ -0,0 +1,21 @@
+package calculadora;
+
+public class Numero {
+	
+	private double valor;
+	
+	public Numero(double valor) {
+		super();
+		this.valor = valor;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+}	
+
